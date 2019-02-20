65e262a9ae5acecc8fe7fb39e8d49829cfc8246e
Ilton <ilton.silva@sankhya.com.br>
2019-02-20 17:02:13 -0300

    Implementa classe Requisitos

---
+++ src/br/com/sankhya/gerenciadorprojetos/dominio/Requisitos.java
@@ -0,0 +1,73 @@
+/*
+Nome do autor: Ilton Batista da Silva Júnior
+Data de criação do arquivo: 20/02/2019
+Objetivo sucinto da classe:Cria uma classe que manipula os requisitos e atualizações de prioridade de um produto
+Escopo do projeto:  https://docs.google.com/document/d/1Hskfyyg0FAgsRGs5d1hBUyV5UH1YGbXMyQM99SFdUWk/edit?usp=sharing
+*/
+
+package br.com.sankhya.gerenciadorprojetos.dominio;
+
+public class Requisitos {
+	private Integer requisitoID;
+	private String descricao;
+	
+	
+	
+	public Requisitos() {
+		super();
+	}
+	
+	public Requisitos(Integer requisitoID, String descricao) {
+		super();
+		this.requisitoID = requisitoID;
+		this.descricao = descricao;
+	}
+	
+	public Integer getRequisitoID() {
+		return requisitoID;
+	}
+	
+	public void setRequisitoID(Integer requisitoID) {
+		this.requisitoID = requisitoID;
+	}
+	
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((requisitoID == null) ? 0 : requisitoID.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Requisitos other = (Requisitos) obj;
+		if (requisitoID == null) {
+			if (other.requisitoID != null)
+				return false;
+		} else if (!requisitoID.equals(other.requisitoID))
+			return false;
+		return true;
+	}
+	
+	@Override
+	public String toString() {
+		return "Requisitos ["
+				+ "requisitoID=" + requisitoID + ", "
+				+ "descricao=" + descricao + "]";
+	}
+	
+}
