7857db29aa7daa05426c127777e6cc6d83b0139c
xZeroLinkx <xZeroLinkx@hotmail.com>
2019-02-20 14:43:45 -0300

    Implementa a classe Cliente e relacionamento de heranca

---
+++ src/br/com/sankhya/gerenciadorprojetos/dominio/Cliente.java
@@ -0,0 +1,69 @@
+/*
+Nome do autor: Elian Melo Morais
+Data de criação do arquivo: 19/02/2019
+Objetivo sucinto da classe: Possui as caracteristicas de um cliente do programa
+Escopo do projeto: https://docs.google.com/document/d/1Hskfyyg0FAgsRGs5d1hBUyV5UH1YGbXMyQM99SFdUWk/edit?usp=sharing
+*/
+package br.com.sankhya.gerenciadorprojetos.dominio;
+
+import java.io.Serializable;
+
+public class Cliente extends Usuario implements Serializable {
+	private static final long serialVersionUID = 1L;
+	private String email;
+	private String telefone;
+	
+	public Cliente() { }
+
+	public Cliente(Integer usuarioID, String nome, String cpf, String email, String telefone) {
+		super(usuarioID, nome, cpf);
+		this.email = email;
+		this.telefone = telefone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + ((getUsuarioID() == null) ? 0 : getUsuarioID().hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		if (getUsuarioID() == null) {
+			if (other.getUsuarioID() != null)
+				return false;
+		} else if (!getUsuarioID().equals(other.getUsuarioID()))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {		
+		return "Cliente [usuarioID=" + getUsuarioID() + ", nome=" + getNome() + ", cpf=" + getCpf() + ", email=" + getEmail() + ", telefone=" + getTelefone() + "]";
+	}
+}
