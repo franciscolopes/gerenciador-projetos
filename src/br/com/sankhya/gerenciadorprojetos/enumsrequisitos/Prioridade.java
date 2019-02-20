c41c703149c645863ef54081f641bf994279403d
Ilton <ilton.silva@sankhya.com.br>
2019-02-20 17:26:03 -0300

    Coloca comentario padrão

--- src/br/com/sankhya/gerenciadorprojetos/enumsrequisitos/Prioridade.java
+++ src/br/com/sankhya/gerenciadorprojetos/enumsrequisitos/Prioridade.java
@@ -1,3 +1,11 @@
+/*
+Nome do autor: Ilton Batista da Silva Júnior
+Data de criação do arquivo:20/02/2019
+Objetivo sucinto do enum: gerenciar a prioridade de um requisito
+Escopo do projeto:  https://docs.google.com/document/d/1Hskfyyg0FAgsRGs5d1hBUyV5UH1YGbXMyQM99SFdUWk/edit?usp=sharing
+*/
+
+
 package br.com.sankhya.gerenciadorprojetos.enumsrequisitos;
 
 public enum Prioridade {
