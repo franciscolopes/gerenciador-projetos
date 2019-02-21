8616ecc1150a53754291d6a110ee45b65a11f911
Ezequiel Oliveira <ezequiel.santana@sankhya.com.br>
2019-02-21 15:19:15 -0300

    Adiciona EstadoProjeto no pacote enums

--- src/br/com/sankhya/gerenciadorprojetos/enums/EstadoProjeto.java
+++ src/br/com/sankhya/gerenciadorprojetos/enums/EstadoProjeto.java
@@ -4,7 +4,7 @@ Data de criação do arquivo: 19/02/2019
 Objetivo sucinto do enum: Cria enum para exibir status do projeto
 Escopo do projeto:  https://docs.google.com/document/d/1Hskfyyg0FAgsRGs5d1hBUyV5UH1YGbXMyQM99SFdUWk/edit?usp=sharing
 */
-package br.com.sankhya.gerenciadorprojetos.dominio;
+package br.com.sankhya.gerenciadorprojetos.enums;
 
 public enum EstadoProjeto {
 	EM_ANDAMENTO(1, "Em_Andamento"), CONCLUIDO(2,"Concluido");
