dfeb6fc0a5b35d019aad8ae1ab7fa95fbe785b1b
Ilton <ilton.silva@sankhya.com.br>
2019-02-21 16:10:44 -0300

    Altera enum Prioridade

--- src/br/com/sankhya/gerenciadorprojetos/enums/Prioridade.java
+++ src/br/com/sankhya/gerenciadorprojetos/enums/Prioridade.java
@@ -6,20 +6,39 @@ Objetivo sucinto do enum: gerenciar a prioridade de um requisito
 Escopo do projeto:  https://docs.google.com/document/d/1Hskfyyg0FAgsRGs5d1hBUyV5UH1YGbXMyQM99SFdUWk/edit?usp=sharing
 */
 
-
 public enum Prioridade {
-	ALTA(1),
-	MEDIA(2),
-	BAIXA(3);
+	ALTA(1, "Alta"),
+	MEDIA(2, "Media"),
+	BAIXA(3, "Baixa");
 	
-	int prioridade;
-
-	private Prioridade(int prioridade) {
-		this.prioridade = prioridade;
+	int codigo;
+	String descricao;
+	
+	private Prioridade(int codigo, String descricao) {
+		this.codigo = codigo;
+		this.descricao = descricao;
 	}
 
-	public int getPrioridade() {
-		return prioridade;
+	public int getCodigo() {
+		return codigo;
 	}
-	
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public static Prioridade toEnum(Integer codigo) {
+		
+		if(codigo == null) {
+			return null;
+		}
+		
+		for(Prioridade x : Prioridade.values()) {
+			if(codigo.equals(x.getCodigo())) {
+				return x;
+			}
+		}
+		
+		throw new IllegalArgumentException("Prioridade inválido: " + codigo);
+	}
 }
