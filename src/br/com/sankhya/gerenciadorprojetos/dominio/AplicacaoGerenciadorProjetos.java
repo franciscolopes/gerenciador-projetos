f6303af8ce143d212c0a5e350ac94cab168ef03a
xZeroLinkx <xZeroLinkx@hotmail.com>
2019-02-22 17:01:33 -0300

    Adiciona o menu cadastrar cliente

--- src/br/com/sankhya/gerenciadorprojetos/dominio/AplicacaoGerenciadorProjetos.java
+++ src/br/com/sankhya/gerenciadorprojetos/dominio/AplicacaoGerenciadorProjetos.java
@@ -7,8 +7,10 @@ import java.util.Scanner;
 public class AplicacaoGerenciadorProjetos {
 	public static List<Cliente> clientes = new ArrayList<>();
 	public static Scanner leitor = new Scanner(System.in);
+	public static Integer usuarioID = 1;
+	public static Integer enderecoID = 1;
 	
-	public static void MenuPrincipal() {
+	public static void menuPrincipal() {
 		while(true) {
 			System.out.println();
 			System.out.println("[ Menu Principal ]");
@@ -17,11 +19,13 @@ public class AplicacaoGerenciadorProjetos {
 				System.out.println("2.Listar Clientes");
 			}
 			
-			System.out.print("\nEscolha uma opção: ");
+			System.out.println();
+			System.out.print("Escolha uma opção: ");
 			int opcao = leitor.nextInt();
+			leitor.nextLine();
 			
 			if(opcao == 1) {
-				// Algo
+				cadastrarCliente();
 			} else if(opcao == 2) {
 				// Algo
 			}  else {
@@ -30,9 +34,54 @@ public class AplicacaoGerenciadorProjetos {
 		}
 	}
 	
+	public static void cadastrarCliente() {
+		while(true) {
+			System.out.println();
+			System.out.println("[ Cadastro de Cliente ]");
+			System.out.println();
+			
+			System.out.print("Nome: ");
+			String nome = leitor.nextLine();
+			System.out.print("CPF: ");
+			String cpf = leitor.nextLine();
+			System.out.print("E-mail: ");
+			String email = leitor.nextLine();
+			System.out.print("Telefone: ");
+			String telefone = leitor.nextLine();
+			
+			System.out.println();
+			System.out.print("Dados da Residência");
+			System.out.println();
+			System.out.println();
+			
+			System.out.print("Logradouro(Rua): ");
+			String logradouro = leitor.nextLine();
+			System.out.print("Número: ");
+			Integer numero = leitor.nextInt();
+			leitor.nextLine();
+			System.out.print("Complemento: ");
+			String complemento = leitor.nextLine();
+			System.out.print("Bairro: ");
+			String bairro = leitor.nextLine();
+			System.out.print("CEP: ");
+			String cep = leitor.nextLine();
+			System.out.print("Cidade: ");
+			String cidade = leitor.nextLine();
+			System.out.print("Estado: ");
+			String estado = leitor.nextLine();
+			
+			Endereco endereco = new Endereco(numero, logradouro, numero, complemento, bairro, cep, cidade, estado);
+			Cliente cliente = new Cliente(numero, nome, cpf, email, telefone);
+			cliente.setEndereco(endereco);
+			clientes.add(cliente);
+			
+			menuPrincipal();
+		}
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("----- Gerenciador de Projetos ------");
-		MenuPrincipal();
+		menuPrincipal();
 	}
 
 }
